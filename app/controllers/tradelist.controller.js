58908d1f520400685a240cb3d2d06619f94833f1
royalmm888 <royalmm888@gmail.com>
2025-07-02 21:35:14 +0700

    second to min

--- app/controllers/tradelist.controller.js
+++ app/controllers/tradelist.controller.js
@@ -548,7 +548,7 @@ exports.createUserTradeConfirm = async (req, res) => {
   // const openingTime = dayjs().tz("Asia/Bangkok").format("YYYY-MM-DD HH:mm:ss");
   // const closingTime = dayjs().tz("Asia/Bangkok").add(req.body.countTime, 'minute').format("YYYY-MM-DD HH:mm:ss");
   const openingTime = dayjs().tz("Asia/Bangkok").toDate();
-  const closingTime = dayjs().tz("Asia/Bangkok").add(req.body.countTime, 'second').toDate();
+  const closingTime = dayjs().tz("Asia/Bangkok").add(req.body.countTime, 'minute').toDate();
 
 
   let user_data = {
